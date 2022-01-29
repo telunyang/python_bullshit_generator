d9181665c948f87602e247e351f02417bcd00883
DarrenYang <telunyang@gmail.com>
2022-01-30 02:45:15 +0800

    執行檔 -> 使用 python run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from Bullshit import Bullshit
+
+'''
+主程式區域
+'''
+if __name__ == "__main__":
+    topic = input("請輸入主題: ")
+    min_length = input("請輸入最少字數: ")
+    obj = Bullshit()
+    print( obj.generate(topic, min_length) )
